7015cad6c58bdf46b97ceae98425455967e0c276
Bruno Frank <bfscordeiro@gmail.com>
2018-11-30 16:02:29 -0200

    Remove generators

--- lib/enumerated_attribute/rails_helpers.rb
+++ lib/enumerated_attribute/rails_helpers.rb
@@ -9,11 +9,11 @@ if defined?(ActiveRecord)
 					column_without_enumerated_attribute(name, type, options)
 				end
 				safe_alias_method_chain :column, :enumerated_attribute
-				
+
 				def enum(*args)
-					options = args.extract_options!                                      
-					column_names = args                                                   
-					column_names.each { |name| column(name, 'string', options) }  
+					options = args.extract_options!
+					column_names = args
+					column_names.each { |name| column(name, 'string', options) }
 				end
 			end
 			class Table
@@ -22,35 +22,12 @@ if defined?(ActiveRecord)
 					column_without_enumerated_attribute(name, type, options)
 				end
 				safe_alias_method_chain :column, :enumerated_attribute
-				
+
 				def enum(*args)
-					options = args.extract_options!                                      
-					column_names = args                                                   
-					column_names.each { |name| column(name, 'string', options) }  
+					options = args.extract_options!
+					column_names = args
+					column_names.each { |name| column(name, 'string', options) }
 				end
-			end
-		end
-	end
-end
-
-#ARGV is used by generators -- if it contains one of these generator commands - add enumeration support
-#unless ((ARGV || []) & ["scaffold", "rspec_scaffold", "nifty_scaffold"]).empty?
-if ((ARGV || []).any?{|o| o =~ /scaffold/ })
-  begin
-    require 'rails/generators'
-    require 'rails/generators/generated_attribute'
-  rescue
-  rescue Exception => ex
-  end
-
-	module Rails
-		module Generators
-			class GeneratedAttribute
-				def field_type_with_enumerated_attribute
-					return (@field_type = :enum_select) if type == :enum
-					field_type_without_enumerated_attribute
-				end
-				alias_method_chain :field_type, :enumerated_attribute
 			end
 		end
 	end
@@ -59,7 +36,7 @@ end
 if defined?(ActionView::Base)
 	module ActionView
 		module Helpers
-		
+
 			#form_options_helper.rb
 			module FormOptionsHelper
 				#def select
@@ -67,7 +44,7 @@ if defined?(ActionView::Base)
 					InstanceTag.new(object, method, self, options.delete(:object)).to_enum_select_tag(options, html_options)
 				end
 			end
-			
+
 			class InstanceTag
 				def to_enum_select_tag(options, html_options={})
 					choices = []
@@ -82,14 +59,14 @@ if defined?(ActionView::Base)
 					end
 					to_select_tag(choices, options, html_options)
 				end
-				
+
 				#initialize record_name, method, self
 				if respond_to?(:to_tag)
   				def to_tag_with_enumerated_attribute(options={})
   					#look for an enum
-  					if (column_type == :string && 
+  					if (column_type == :string &&
   						self.object.class.respond_to?(:has_enumerated_attribute?) &&
-  						self.object.class.has_enumerated_attribute?(method_name.to_sym)) 
+  						self.object.class.has_enumerated_attribute?(method_name.to_sym))
   						to_enum_select_tag(options)
   					else
   						to_tag_without_enumerated_attribute(options)
@@ -98,15 +75,13 @@ if defined?(ActionView::Base)
   				alias_method_chain :to_tag, :enumerated_attribute
 				end
 			end
-			
+
 			class FormBuilder
 				def enum_select(method, options={}, html_options={})
 					@template.enum_select(@object_name, method, objectify_options(options), @default_options.merge(html_options))
 				end
 			end
-			
+
 		end
 	end
 end
-
-
